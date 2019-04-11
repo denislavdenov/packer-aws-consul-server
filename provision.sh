ddbe5d0a9d68f48cbc85b554aedab41b83d6ad23
Denov <denov@Denovs-MacBook-Pro.local>
2019-04-11 14:43:02 +0300

    pffff

--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 sudo mkdir -p /etc/dpkg/dpkg.cfg.d
-sudo cat >/etc/dpkg/dpkg.cfg.d/01_nodoc <<EOF
+sudo tee /etc/dpkg/dpkg.cfg.d/01_nodoc << EOF
 path-exclude /usr/share/doc/*
 path-include /usr/share/doc/*/copyright
 path-exclude /usr/share/man/*
@@ -20,17 +21,26 @@ sudo apt-get update ${APTARGS}
 sudo apt-get upgrade -y ${APTARGS}
 sudo apt-get dist-upgrade -y ${APTARGS}
 
-
 which unzip socat jq dnsutils net-tools vim curl sshpass &>/dev/null || {
 sudo apt-get update -y ${APTARGS}
 sudo apt-get install unzip socat jq dnsutils net-tools vim curl sshpass -y ${APTARGS}
 }
+
 # install consul
+mkdir -p /tmp/pkg/
 if [ -z "$CONSUL_VER" ]; then
     CONSUL_VER=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
 fi
-mkdir -p /tmp/pkg/
-curl -s https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip
+response=$(curl -LI https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip -w '%{http_code}\n' -s)
+
+if [ $response == 200 ]; then
+    curl -s https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip
+else
+   exit 1
+fi
+
+
+
 echo "Installing Consul version ${CONSUL_VER} ..."
 pushd /tmp
 unzip /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip 
